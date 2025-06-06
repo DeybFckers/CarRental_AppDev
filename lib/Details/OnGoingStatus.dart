e4c09033946e5dc3fcc25fdcf5745e381e2789e2
DeybFckers <pucklord322@gmail.com>
2025-06-07 01:13:40 +0800

    user inbox and cancelled not done

--- lib/Details/OnGoingStatus.dart
+++ lib/Details/OnGoingStatus.dart
@@ -1,14 +1,9 @@
-import 'dart:convert';
 import 'package:CarRentals/api_connection/LoggedInUser.dart';
-import 'package:CarRentals/api_connection/getRental.dart';
 import 'package:CarRentals/consts/ReuseableClass.dart';
-import 'package:CarRentals/successScreens/Cancel_success.dart';
 import 'package:flutter/material.dart';
 import 'package:CarRentals/api_connection/BookingDetails.dart';
 import 'package:iconsax/iconsax.dart';
-import 'package:http/http.dart' as http;
-import 'package:CarRentals/api_connection/api_connection.dart';
-import 'package:get/get.dart';
+
 
 class OnGoingStatus extends StatefulWidget {
   final BookingDetails booking;
@@ -22,42 +17,6 @@ class OnGoingStatus extends StatefulWidget {
 }
 
 class _BookingDetailsPage extends State<OnGoingStatus>{
-  Future<void> cancelBooking() async{
-    final booking = widget.booking;
-
-    DateTime startDate = DateTime.parse(booking.preferredStartDate);
-    DateTime endDate = DateTime.parse(booking.preferredEndDate);
-    int days = endDate.difference(startDate).inDays + 1;
-
-    double totalPrice = booking.dailyRate * days;
-
-    RentalCancel rentalCancel = RentalCancel(
-      customerID: widget.user.Customer_ID,
-      carID: booking.carId,
-      startDate: booking.preferredStartDate,
-      endDate: booking.preferredEndDate,
-      totalPrice: totalPrice,
-      rentalStatus: "Cancelled",
-    );
-
-    try{
-      var res = await http.post(
-        Uri.parse(API.cancelRental),
-        body: rentalCancel.toJson(),
-      );
-      if(res.statusCode == 200){
-        var resBodyOfCancel = jsonDecode(res.body);
-        if(resBodyOfCancel['Success'] == true){
-          Get.off(() => CancelSuccessScreen(user: widget.user));
-        } else {
-
-        }
-      }
-    }catch(e){
-
-    }
-
-  }
 
   @override
   Widget build(BuildContext context) {
--- lib/screens/BookingStatusScreen.dart
+++ lib/screens/BookingStatusScreen.dart
@@ -36,6 +36,7 @@ class BookingStatusScreen extends StatelessWidget {
           children: [
             PendingTab(user: user),
             OnGoingTab(user: user),
+            CompletedTab(user: user)
           ],
         ),
       ),
@@ -163,6 +164,111 @@ class OnGoingTab extends StatelessWidget{
           .map((item)=>BookingDetails.fromJson(item))
           .where((booking) => booking.customerName == user.Customer_Name &&
           booking.rentalStatus == 'Confirmed')
+          .toList();
+    }else{
+      return [];
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: FutureBuilder<List<BookingDetails>>(
+          future: fetchBookings(),
+          builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return Center(child: CircularProgressIndicator());
+            }else if (snapshot.hasError){
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
+
+            final bookings =snapshot.data!;
+            if (bookings.isEmpty){
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'No Bookings found.',
+                      style: TextStyle(fontSize: 20, color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 20,),
+                    ElevatedButton(
+                      onPressed: () {
+                        final controller = Get.find<NavigationController>();
+                        controller.selectedIndex.value = 0;
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.yellow[900],
+                          fixedSize: Size(300, 55)
+                      ),
+                      child: Text('Book a car',
+                          style: TextStyle(
+                              fontSize: 25,
+                              color: Colors.white)
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            }
+
+            return ListView.builder(
+              itemCount: bookings.length,
+              itemBuilder: (context, index) {
+                final booking = bookings[index];
+                return InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => OnGoingStatus(booking: booking,
+                              user: user),
+                        ),
+                      );
+                    },
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: ListTile(
+                        title: Text('${booking.brand} ${booking.model}',
+                          style: TextStyle(color: Colors.white, fontWeight:
+                          FontWeight.bold),
+                        ),
+                        subtitle: Text(
+                          'From ${booking.preferredStartDate} to ${booking
+                              .preferredEndDate}',
+                        ),
+                        trailing: Text(booking.rentalStatus,
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
+                        ),
+                      ),
+                    )
+                );
+              },
+            );
+          },
+        )
+    );
+  }
+}
+
+//CompletedStatus
+class CompletedTab extends StatelessWidget{
+  final LoggedInUser user;
+  const CompletedTab({super.key, required this.user});
+
+  Future<List<BookingDetails>> fetchBookings() async{
+    final response = await http.get(Uri.parse(API.getbookingdetails));
+
+    if(response.statusCode == 200){
+      List<dynamic> jsonData = jsonDecode(response.body);
+      return jsonData
+          .map((item)=>BookingDetails.fromJson(item))
+          .where((booking) => booking.customerName == user.Customer_Name &&
+          booking.rentalStatus == 'Completed')
           .toList();
     }else{
       return [];
